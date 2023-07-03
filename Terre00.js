cc8ad2ab4205bc43f01c9e914cd79e4f1585a973
Hugo Gaupillat <110528656+Tallipaug@users.noreply.github.com>
2023-07-03 11:44:48 +0200

    Add files via upload

---
+++ Terre00.js
@@ -0,0 +1,8 @@
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+let stock  = '' ;
+
+for (let i = 0; i < letters.length; i++) {
+    stock += letters[i] + '';
+}
+
+console.log(stock);
