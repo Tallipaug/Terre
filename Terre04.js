07d62da00ab634c4b256b900a08d30227a7d619d
Hugo Gaupillat <110528656+Tallipaug@users.noreply.github.com>
2023-08-30 10:30:07 +0200

    Add files via upload

---
+++ Terre04.js
@@ -0,0 +1,26 @@
+const number= "string";
+
+if (number == "string" ){
+  console.log("je ne suis pas ton père mais tu te trompes");
+}
+
+if( number % 2 ===0){
+  console.log("nombre pair");
+}
+else
+ console.log("impair");
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
